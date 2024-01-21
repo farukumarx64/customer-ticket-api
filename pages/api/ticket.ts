03c5f930d05b0f4fbd27d49e7ce16b4a211c26fd
farukumarx64 <f.umaridris.mail@gmail.com>
2024-01-21 14:49:59 +0100

    second commit

--- pages/api/ticket.ts
+++ pages/api/ticket.ts
@@ -13,7 +13,7 @@ export default async function handler(
 ) {
   try {
     const projectRoot = process.cwd();
-    const scriptPath = join(projectRoot, 'pages', 'api', 'generate-ticket', 'generate-ticket');
+    const scriptPath = join(__dirname, 'generate-ticket', 'generate-ticket');
     console.log(scriptPath)
 
     const { stdout } = await new Promise<{ stdout: string }>((resolve, reject) => {
